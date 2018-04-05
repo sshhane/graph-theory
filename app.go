86df270668407db1ac0f8e34f3d3d2c812778532
sshhane <shaned257@gmail.com>
2018-04-05 13:25:31 +0100

    infix / postfix

--- app.go
+++ app.go
@@ -1,18 +1,37 @@
-type nfa struct {
-    // ... 
+package main
+
+import (
+	"fmt"
+)
+
+func intopost(infix string) string {
+	postfix := ""
+	return postfix
 }
 
-func regexcompile(r string) nfa {
-    // ...
-    return n
-}
-
-func (n nfa) regexmatch(n nfa, r sting) bool {
-    // ... 
-    return ismatch 
-}
 func main() {
-    n := regexcompile("01*0")
-    t := n.regexmatch("01110")
-    f := n.regexmatch("1000001") 
+
+	//ab.c*
+	fmt.Println("infix:  ", "a.b.c*")
+	fmt.Println("postfix:  ", intopost("a.b.c*"))
+
 }
+
+// type nfa struct {
+//     // ...
+// }
+
+// func regexcompile(r string) nfa {
+//     // ...
+//     return n
+// }
+
+// func (n nfa) regexmatch(n nfa, r sting) bool {
+//     // ...
+//     return ismatch
+// }
+// func main() {
+//     n := regexcompile("01*0")
+//     t := n.regexmatch("01110")
+//     f := n.regexmatch("1000001")
+// }
