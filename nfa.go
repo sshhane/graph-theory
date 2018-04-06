974f72a9e9f0f28896426ac26732a90db46acc7b
sshhane <shaned257@gmail.com>
2018-04-06 14:21:29 +0100

    added comments

--- nfa.go
+++ nfa.go
@@ -21,30 +21,33 @@ func poregtonfa(posfix string) *nfa {
 	for _, r := range posfix {
 		switch r {
 		case '.':
-
-			frag2 := nfastack[len(nfastack)-1]
+			// first is at bottom
+			frag2 := nfastack[len(nfastack)-1] // pop last off stack
 			nfastack = nfastack[:len(nfastack)-1]
 			frag1 := nfastack[len(nfastack)-1]
 			nfastack = nfastack[:len(nfastack)-1]
 
+			// concat accept state of first to initial of second
 			frag1.accept.edge1 = frag2.initial
 
-			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept}) // frag2 accept state is accept state of new fragment
 		case '|':
 			frag2 := nfastack[len(nfastack)-1]
 			nfastack = nfastack[:len(nfastack)-1]
 			frag1 := nfastack[len(nfastack)-1]
 			nfastack = nfastack[:len(nfastack)-1]
 
+			// new initial state
 			initial := state{edge1: frag1.initial, edge2: frag2.initial}
 
+			// join accept states to new state
 			accept := state{}
 			frag1.accept.edge1 = &accept
 			frag2.accept.edge1 = &accept
 
 			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
 		case '*':
-
+			// pop one frag off stack
 			frag := nfastack[len(nfastack)-1]
 			nfastack = nfastack[:len(nfastack)-1]
 
@@ -54,12 +57,15 @@ func poregtonfa(posfix string) *nfa {
 			frag.accept.edge1 = frag.initial
 			frag.accept.edge2 = &accept
 
+			// push new frag
+			// old frag with new accept and initial states
 			nfastack = append(nfastack, &nfa{initial: &accept, accept: &accept})
 
 		default:
 			accept := state{}
 			initial := state{symbol: r, edge1: &accept}
 
+			// push to stack
 			nfastack = append(nfastack, &nfa{initial: &accept, accept: &accept})
 
 		}
